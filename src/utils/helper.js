156ccd8a056e2ceda0c783c193bbfc3272e19aa9
Sk. Imran Hussain <skimranpc@gmail.com>
2023-11-16 22:39:14 +0530

    helper.js file for utility functions

---
+++ src/utils/helper.js
@@ -0,0 +1,6 @@
+export function filterCards(searchInput, restaurants){
+    const filteredData = restaurants?.filter((restaurant) => {
+       return restaurant?.info?.name?.toLowerCase().includes(searchInput.toLowerCase())
+    })
+    return filteredData;
+}
