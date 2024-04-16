7adc85d6a647c14819f3a0cca56bb590b9fe15fc
Sk. Imran Hussain <skimranpc@gmail.com>
2024-04-16 06:22:34 +0530

    accordian built and styled in about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,45 @@
 import { Outlet } from "react-router-dom";
+import { aboutMe, aboutUs, featuresDesc } from "../Constant";
+import { useState } from "react";
+
+const Section = ({title, description}) =>{
+    const [isVisible, setIsVisible] = useState(false)
+
+   return (
+   <div className="my-4">
+    <div className={`flex justify-between items-center bg-stone-700 pl-4 rounded-t-md  ${!isVisible ? "border-b-4 border-myYellow" : "border-none"}`}>
+        <h2 className={`text-2xl font-bold py-1 font-serif transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`}>{title}</h2>
+        {
+          !isVisible ?  
+          <i class="fa-solid fa-angle-down text-2xl pr-4 cursor-pointer transition-all duration-300" onClick={() =>{
+            setIsVisible(!isVisible)
+          }}></i> 
+          :
+          <i class="fa-solid fa-angle-up text-2xl pr-4 cursor-pointer transition-all duration-300" onClick={() =>{
+            setIsVisible(!isVisible)
+          }}></i>
+        } 
+    </div>
+    <div className={`bg-stone-600 rounded-b-md ${isVisible ? "border-b-8 border-myYellow" : "border-none"}`}>
+    {
+        isVisible && <p className="font-thin text-base ease-out duration-1000 px-4 py-2 text-white font-serif">{description}</p>
+    }
+    </div>
+
+     </div>
+  
+)
+}
 
 const About = () => {
     return(
-        <div>
-            <h1>This is about page.</h1>
+        <div className="w-3/5 m-auto my-4 min-h-full">
+            <Section title={"Features"} description={featuresDesc}/>
+            <Section title={"About Us"} description={aboutUs}/>
+            <Section title={"About Me"} description={aboutMe}/>
+
             <Outlet />
+
         </div>
     )
 }
--- src/Components/ShimmerUI.js
+++ src/Components/ShimmerUI.js
@@ -6,7 +6,7 @@ const Shimmer = ({ count }) => {
   for (let i = 0; i < count; i++) {
     shimmerElements.push(
       <div className="shimmer-container" key={i}>
-        <div className="shimmer w-60 h-72 flex flex-col gap-2">
+        <div className="shimmer w-60 h-72 flex flex-col gap-2 animate-pulse">
           <div id="sh-1" className="w-[100%] h-36 bg-[#8080805b] rounded-md"></div>
           <div id="sh-2" className="w-[80%] h-12 bg-[#8080805b] pl-2"></div>
           <div id="sh-3" className="w-[50%] h-12 bg-[#8080805b] pl-2"></div>
