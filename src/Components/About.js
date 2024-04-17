68251dcb0522d044ade3db4b49f52743962855e3
Sk. Imran Hussain <skimranpc@gmail.com>
2024-04-17 23:34:43 +0530

    collapsable accordian v.1

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,21 +2,23 @@ import { Outlet } from "react-router-dom";
 import { aboutMe, aboutUs, featuresDesc } from "../Constant";
 import { useState } from "react";
 
-const Section = ({title, description}) =>{
-    const [isVisible, setIsVisible] = useState(false)
+const Section = ({title, description, isVisible, setIsVisible}) =>{
+    
 
    return (
    <div className="my-4">
     <div className={`flex justify-between items-center bg-stone-700 pl-4 rounded-t-md  ${!isVisible ? "border-b-4 border-myYellow" : "border-none"}`}>
-        <h2 className={`text-2xl font-bold py-1 font-serif transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`}>{title}</h2>
+        <h2 className={`text-2xl font-bold py-1 font-serif cursor-pointer transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`} onClick={() =>{
+            setIsVisible()
+          }}>{title}</h2>
         {
           !isVisible ?  
-          <i class={`fa-solid fa-angle-down text-2xl pr-4 cursor-pointer transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`} onClick={() =>{
-            setIsVisible(!isVisible)
+          <i className={`fa-solid fa-angle-down text-2xl pr-4 cursor-pointer transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`} onClick={() =>{
+            setIsVisible()
           }}></i> 
           :
-          <i class={`fa-solid fa-angle-up text-2xl pr-4 cursor-pointer transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`} onClick={() =>{
-            setIsVisible(!isVisible)
+          <i className={`fa-solid fa-angle-up text-2xl pr-4 cursor-pointer transition-all duration-300 ${isVisible ? "text-white" : "text-stone-300"}`} onClick={() =>{
+            setIsVisible()
           }}></i>
         } 
     </div>
@@ -32,11 +34,56 @@ const Section = ({title, description}) =>{
 }
 
 const About = () => {
+  const [visibleSection, setVisibleSection] = useState({
+    "showFeature" : true,
+    "showAboutUs" : false,
+    "showAboutMe" : false
+  })
+
     return(
         <div className="w-3/5 m-auto my-4 min-h-full">
-            <Section title={"Features"} description={featuresDesc}/>
-            <Section title={"About Us"} description={aboutUs}/>
-            <Section title={"About Me"} description={aboutMe}/>
+            <Section title={"Features"} description={featuresDesc} isVisible={visibleSection.showFeature} setIsVisible={() => setVisibleSection(
+              visibleSection.showFeature === false ?
+              {
+              "showFeature" : true,
+              "showAboutUs" : false,
+              "showAboutMe" : false
+            }
+            :
+            {
+              "showFeature" : false,
+              "showAboutUs" : false,
+              "showAboutMe" : false
+            }
+          )}/>
+            <Section title={"About Us"} description={aboutUs} isVisible={visibleSection.showAboutUs} setIsVisible={() => setVisibleSection(
+              visibleSection.showAboutUs === false ?
+              {
+              "showFeature" : false,
+              "showAboutUs" : true,
+              "showAboutMe" : false
+            }
+            :
+            {
+              "showFeature" : false,
+              "showAboutUs" : false,
+              "showAboutMe" : false
+            }
+          )}/>
+            <Section title={"About Me"} description={aboutMe} isVisible={visibleSection.showAboutMe} setIsVisible={() => setVisibleSection(
+              visibleSection.showAboutMe === false ?
+              {
+              "showFeature" : false,
+              "showAboutUs" : false,
+              "showAboutMe" : true
+            }
+            :
+            {
+              "showFeature" : false,
+              "showAboutUs" : false,
+              "showAboutMe" : false
+            }
+          )}/>
 
             <Outlet />
 
